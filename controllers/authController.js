603cb4b74286e2014f01f4163d219ebb274c574d
Bradley Pagano <bpagano5243@gmail.com>
2020-01-03 13:21:20 -0500

    adds validation errors to signUpForm

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,42 +2,44 @@ const jwt = require("jsonwebtoken");
 const db = require("../models");
 
 module.exports = {
-    create: function (req, res) {
-        db.User
-            .create(req.body)
-            .then(function (result) {
-                res.json({ message: result.username + " Created" })
-            })
-            .catch(function (err) {
-                res.status(500).json({ error: err.message })
-            })
-    },
-    check: function (req, res) {
-        const { username, password } = req.body
-        db.User
-            .findOne({ username: username })
-            .then(function (dbUser) {
-                if (!dbUser) return res.status(401).json({ message: "Username or Password is incorrect." })
-                if (dbUser.comparePassword(password)) {
-                    const token = jwt.sign({
-                        data: dbUser._id
-                    }, process.env.KEY)
+  create: function(req, res) {
+    db.User.create(req.body)
+      .then(function(result) {
+        res.json({ message: result.username + " Created" });
+      })
+      .catch(function(err) {
+        res.status(500).json({ error: err.message });
+      });
+  },
+  check: function(req, res) {
+    const { username, password } = req.body;
+    db.User.findOne({ username: username }).then(function(dbUser) {
+      console.log(dbUser);
+      if (!dbUser)
+        return res
+          .status(401)
+          .json({ message: "Username or Password is incorrect." });
+      if (dbUser.comparePassword(password)) {
+        const token = jwt.sign(
+          {
+            data: dbUser._id
+          },
+          process.env.KEY
+        );
 
-                    res.json({
-                        id: dbUser._id,
-                        username: dbUser.username,
-                        token: token
-                    })
-                } else {
-                    res.status(401).json({ message: "Username or password is incorrect." });
-                }
-            })
-    },
-    userGet: function (req, res) {
-        db.User
-            .findById(req._id)
-            .then(dbUser => {
-                res.json(dbUser);
-            })
-    }
-}
+        res.json({
+          id: dbUser._id,
+          username: dbUser.username,
+          token: token
+        });
+      } else {
+        res.status(401).json({ message: "Username or password is incorrect." });
+      }
+    });
+  },
+  userGet: function(req, res) {
+    db.User.findById(req._id).then(dbUser => {
+      res.json(dbUser);
+    });
+  }
+};
